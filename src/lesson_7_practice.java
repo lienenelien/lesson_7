4f6875eefa079c9970903e6f2dfb6e2c7c356d1d
lienenelien <liene.lagzdina25@gmail.com>
2021-07-12 19:40:47 +0300

    Initial commit

---
+++ src/lesson_7_practice.java
@@ -0,0 +1,54 @@
+public class lesson_7_practice {
+    public static void main(String[] args) {
+
+
+//        //Alarm clock application
+//        String dayOfWeek;
+//
+//        //initialization
+//        dayOfWeek = "Holiday";
+//
+//        if (dayOfWeek == "Monday" || dayOfWeek == "Tuesday") {
+//        System.out.println("Alarm is set to 7 o'clock");
+//        }else if (dayOfWeek == "Wednesday") {
+//        System.out.println("Alarm is set to 8 o'clock");
+//        }else if (dayOfWeek == "Thursday" || dayOfWeek ==  "Friday") {
+//            System.out.println("Alarm is set to 7:30");
+//        }else if ( dayOfWeek == "Saturday" || dayOfWeek == "Sunday") {
+//            System.out.println("No alarm is set");
+//        }else {
+//            System.out.println("No such day of week exists");
+//        }
+
+        //If you go to office
+        //Working day -  smart casual
+        //Casual Friday - casual
+        //Holidays - wear whatever
+        //Halloween - wear any costume
+
+        String day = "Working day";
+                if (day == "Working day") {
+                    System.out.println("Smart casual attire.");
+                }
+                else if (day == "Casual Friday") {
+                    System.out.println("Casual attire.");
+                }
+                else if ( day == "Holiday") {
+                    System.out.println("Wear anything You please!");
+                }
+                else if ( day == "Halloween") {
+                    System.out.println("Wear your favourite costume!");
+                }
+                else {
+                    System.out.println("Such day does not exist");
+                }
+
+
+
+
+
+
+
+
+    }
+}
